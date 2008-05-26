08db319d06190a44de04b8565924cfcc980f3939
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2008-05-26 18:23:31 +0000

    @chg 	rename Namecheck-->NameCreator
    git-svn-id: file:///SVN_Archive/Software/trunk@2650 9b6744e0-6d27-0410-8403-e1b9a971d29b

---
+++ src/eu/simuline/names/NameCreator.java
@@ -0,0 +1,51 @@
+
+package eu.simuline.names;
+
+import eu.simuline.names.parser.ParseException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+/**
+ * Describe class NameCreator here.
+ *
+ *
+ * Created: Mon Apr 14 18:02:24 2008
+ *
+ * @author <a href="mailto:ernst@">Ernst Reissner</a>
+ * @version 1.0
+ */
+public class NameCreator {
+
+
+    private Files files;
+
+    /**
+     * Creates a new <code>NameCreator</code> instance.
+     *
+     */
+    public NameCreator(File rules) 
+	throws FileNotFoundException, IOException, ParseException {
+	this.files = new Files(rules);	    
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+	if (args.length != 1) {
+	   throw new IllegalArgumentException
+	       ("Usage: the name of the rules file. ");
+	}
+
+	NameCreator nCheck = new NameCreator(new File(args[0]));
+	CatGrammar catGr = nCheck.files.catGr;
+	CheckerFrame frame = new CheckerFrame(catGr);
+	frame.setCats(catGr.starts);
+	frame.setVisible(true);
+System.out.println("finished");
+
+    }
+
+}
