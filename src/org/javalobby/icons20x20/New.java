cf1e16385db73a98fb477ea3679d17ed328058c5
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2008-06-09 22:35:25 +0000

    @new 	activated new icons used for namechecker/analyzer 	for MAN Diesel SE
    git-svn-id: file:///SVN_Archive/Software/trunk@2773 9b6744e0-6d27-0410-8403-e1b9a971d29b

---
+++ src/org/javalobby/icons20x20/New.java
@@ -0,0 +1,16 @@
+package org.javalobby.icons20x20;
+
+import  eu.simuline.util.GifResource;
+
+/**
+ * Represents a "new" icon. 
+ *
+ *
+ * Created: Sun Jun  4 20:50:12 2006
+ *
+ * @author <a href="mailto:ernst@">Ernst Reissner</a>
+ * @version 1.0
+ */
+public class New extends GifResource {
+
+}
