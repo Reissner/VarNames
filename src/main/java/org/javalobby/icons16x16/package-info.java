5fb2d54f452cc21719f3626091db21d5a816b351
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2017-11-09 07:00:19 +0000

    @doc 	added package info.
    git-svn-id: file:///SVN_Archive/Software/trunk@12455 9b6744e0-6d27-0410-8403-e1b9a971d29b

---
+++ src/main/java/org/javalobby/icons16x16/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes which describe icons in gtk-style. 
+ *
+ * <code>x</code> ***** NOT YET COMPLETE. 
+ */
+package org.javalobby.icons16x16;
---
+++ src/main/java/org/javalobby/icons20x20/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes which describe icons in format 20x20. 
+ *
+ * <code>x</code> ***** NOT YET COMPLETE. 
+ */
+package org.javalobby.icons20x20;
