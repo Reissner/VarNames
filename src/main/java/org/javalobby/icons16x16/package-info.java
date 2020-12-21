fbe81aa665e6e6a5e86761632cc12789e3235a24
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2020-12-21 21:41:19 +0000

    from arithmetics deleted classes representing icons (GifResources)
    git-svn-id: file:///SVN_Archive/Software/trunk@13154 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/main/java/org/javalobby/icons16x16/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Classes which describe icons in gtk-style. 
- *
- * <code>x</code> ***** NOT YET COMPLETE. 
- */
-package org.javalobby.icons16x16;
--- src/main/java/org/javalobby/icons20x20/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Classes which describe icons in format 20x20. 
- *
- * <code>x</code> ***** NOT YET COMPLETE. 
- */
-package org.javalobby.icons20x20;
